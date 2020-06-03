e6b015d8feb0cff21ec2f9c262d33daa75a24446
vauxhall <zero.griffin@gmail.com>
2020-06-03 18:35:18 +0200

    Add duration to bar

--- macOS/Bar.swift
+++ macOS/Bar.swift
@@ -3,12 +3,15 @@ import Combine
 
 final class Bar: NSView {
     private weak var base: NSView!
+    private weak var border: NSView!
     private var subs = Set<AnyCancellable>()
+    private let formatter = DateComponentsFormatter()
     
     required init?(coder: NSCoder) { nil }
     init() {
         super.init(frame: .zero)
         translatesAutoresizingMaskIntoConstraints = false
+        formatter.allowedUnits = [.minute, .second]
         
         let base = NSView()
         base.translatesAutoresizingMaskIntoConstraints = false
@@ -17,11 +20,24 @@ final class Bar: NSView {
         addSubview(base)
         self.base = base
         
-        let title = Label("", .regular(10))
+        let border = NSView()
+        border.translatesAutoresizingMaskIntoConstraints = false
+        border.wantsLayer = true
+        base.addSubview(border)
+        self.border = border
+        
+        let title = Label("", .regular(11))
         title.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
         title.maximumNumberOfLines = 1
         title.lineBreakMode = .byTruncatingTail
+        title.textColor = .secondaryLabelColor
         base.addSubview(title)
+        
+        let time = Label("", NSFont(descriptor: NSFont.regular(11).fontDescriptor.addingAttributes([
+        .featureSettings: [[NSFontDescriptor.FeatureKey.selectorIdentifier: kMonospacedNumbersSelector,
+                            .typeIdentifier: kNumberSpacingType]]]), size: 0)!)
+        time.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        base.addSubview(time)
         
         let separator = Separator()
         addSubview(separator)
@@ -31,14 +47,19 @@ final class Bar: NSView {
         base.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         base.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         base.heightAnchor.constraint(equalToConstant: 24).isActive = true
-        base.widthAnchor.constraint(greaterThanOrEqualToConstant: 180).isActive = true
-        let width = base.widthAnchor.constraint(lessThanOrEqualTo: widthAnchor, multiplier: 0.3)
-        width.priority = .defaultLow
-        width.isActive = true
+        base.widthAnchor.constraint(lessThanOrEqualTo: widthAnchor, multiplier: 0.4).isActive = true
+        
+        border.leftAnchor.constraint(equalTo: base.leftAnchor).isActive = true
+        border.rightAnchor.constraint(equalTo: base.rightAnchor).isActive = true
+        border.topAnchor.constraint(equalTo: base.topAnchor).isActive = true
+        border.heightAnchor.constraint(equalToConstant: 1).isActive = true
         
         title.leftAnchor.constraint(equalTo: base.leftAnchor, constant: 10).isActive = true
-        title.rightAnchor.constraint(lessThanOrEqualTo: base.rightAnchor, constant: -10).isActive = true
+        title.rightAnchor.constraint(lessThanOrEqualTo: time.leftAnchor, constant: -10).isActive = true
         title.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        time.rightAnchor.constraint(equalTo: base.rightAnchor, constant: -10).isActive = true
+        time.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         
         separator.heightAnchor.constraint(equalToConstant: 1).isActive = true
         separator.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
@@ -47,10 +68,12 @@ final class Bar: NSView {
         
         playback.sink {
             title.stringValue = .key("track_\($0.track)_composer") + " - " + .key("track_\($0.track)_title")
+            time.stringValue = self.formatter.string(from: $0.elapsed)! + " / " + self.formatter.string(from: $0.track.duration)!
         }.store(in: &subs)
     }
     
     override func updateLayer() {
         base.layer!.backgroundColor = NSColor.controlHighlightColor.cgColor
+        border.layer!.backgroundColor = NSColor.controlLightHighlightColor.cgColor
     }
 }
