c8903953e695338e02a95e0d6e7dce48b3801a78
vauxhall <zero.griffin@gmail.com>
2020-06-09 08:20:57 +0200

    Add notifications support

--- macOS/App.swift
+++ macOS/App.swift
@@ -1,10 +1,11 @@
 import AppKit
 import Combine
+import UserNotifications
 
 let session = Session()
 let playback = Playback()
 
-@NSApplicationMain final class App: NSApplication, NSApplicationDelegate {
+@NSApplicationMain final class App: NSApplication, NSApplicationDelegate, UNUserNotificationCenterDelegate {
     private var subs = Set<AnyCancellable>()
     
     required init?(coder: NSCoder) { nil }
@@ -28,5 +29,39 @@ let playback = Playback()
             window.delegate = window
             session.loadPlayer()
         }.store(in: &subs)
+    }
+    
+    func applicationDidFinishLaunching(_: Notification) {
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().getNotificationSettings {
+            if $0.authorizationStatus != .authorized {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 20) {
+                    UNUserNotificationCenter.current().requestAuthorization(options: [.alert]) { _, _ in }
+                }
+            }
+        }
+        
+        playback.player.track.sink { track in
+            guard playback.playing.value else { return }
+            UNUserNotificationCenter.current().getNotificationSettings {
+                guard $0.authorizationStatus == .authorized else { return }
+                UNUserNotificationCenter.current().add({
+                    $0.title = track.composer.name
+                    $0.body = track.title
+                    return .init(identifier: UUID().uuidString, content: $0, trigger: nil)
+                } (UNMutableNotificationContent()))
+            }
+        }.store(in: &subs)
+    }
+    
+    func userNotificationCenter(_: UNUserNotificationCenter, willPresent: UNNotification, withCompletionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        withCompletionHandler([.alert])
+        UNUserNotificationCenter.current().getDeliveredNotifications {
+            UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: $0.map {
+                $0.request.identifier
+            }.filter {
+                $0 != willPresent.request.identifier
+            })
+        }
     }
 }
