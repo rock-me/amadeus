1ad5e552db99889d42be14cbae4bf06d4aa94808
vauxhall <zero.griffin@gmail.com>
2020-06-10 09:46:17 +0200

    Add music

---
+++ iOS/Music.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+final class Music: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
--- iOS/Scene.swift
+++ iOS/Scene.swift
@@ -3,11 +3,15 @@ import UIKit
 final class Scene: UINavigationController, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo: UISceneSession, options: UIScene.ConnectionOptions) {
         setNavigationBarHidden(true, animated: false)
-        setViewControllers([UIViewController()], animated: false)
+        setViewControllers([Music()], animated: false)
         let app = UIApplication.shared.delegate as! App
         app.windowScene = scene as? UIWindowScene
         app.rootViewController = self
         app.makeKeyAndVisible()
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        //notifications
     }
 }
 
