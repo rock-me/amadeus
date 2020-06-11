e8f521a1aff16a4693b91b4244ac2f689b8093ad
vauxhall <zero.griffin@gmail.com>
2020-06-11 09:43:39 +0200

    Load session

--- iOS/Music.swift
+++ iOS/Music.swift
@@ -1,6 +1,9 @@
 import UIKit
+import Combine
 
 final class Music: UIViewController {
+    private var subs = Set<AnyCancellable>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -13,6 +16,10 @@ final class Music: UIViewController {
         bar.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         bar.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         bar.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+        session.loadUI.sink { _ in
+            session.loadPlayer()
+        }.store(in: &subs)
     }
     
     @objc private func hud() {
--- iOS/Scene.swift
+++ iOS/Scene.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Combine
 
 let session = Session()
 
