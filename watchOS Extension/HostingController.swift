74c1fcefbfbf3848f64fb21422381df58291dfa3
vauxhall <zero.griffin@gmail.com>
2020-06-13 13:20:27 +0200

    Add watch extension

---
+++ watchOS Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  watchOS Extension
+//
+//  Created by vaux on 13.06.20.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
