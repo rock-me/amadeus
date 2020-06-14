f49793a7ef36ce502bcb260f6aee0b056418739a
vauxhall <zero.griffin@gmail.com>
2020-06-14 14:04:11 +0200

    Show items

--- watchOS Extension/Controller.swift
+++ watchOS Extension/Controller.swift
@@ -1,5 +1,18 @@
 import SwiftUI
+import Player
 
-final class Controller: WKHostingController<Music> {
-    override var body: Music { Music() }
+final class MistsController: WKHostingController<Music> {
+    override var body: Music { Music(album: .mists) }
+}
+
+final class MelancholyController: WKHostingController<Music> {
+    override var body: Music { Music(album: .melancholy) }
+}
+
+final class TensionController: WKHostingController<Music> {
+    override var body: Music { Music(album: .tension) }
+}
+
+final class DawnController: WKHostingController<Music> {
+    override var body: Music { Music(album: .dawn) }
 }
--- watchOS Extension/Music.swift
+++ watchOS Extension/Music.swift
@@ -2,23 +2,64 @@ import SwiftUI
 import Player
 
 struct Music: View {
+    let album: Album
+    
     var body: some View {
         GeometryReader { geometry in
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: min(geometry.size.width, geometry.size.height) * 0.1) {
-                    Spacer().frame(width: min(geometry.size.width, geometry.size.height) * 0.2)
-                    ForEach(Album.allCases, id: \.self) {
-                        Image($0.cover)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: (min(geometry.size.width, geometry.size.height) * 0.6) - 2,
-                                   height: (min(geometry.size.width, geometry.size.height) * 0.6) - 2)
-                            .cornerRadius(((min(geometry.size.width, geometry.size.height) * 0.6) - 2) / 2)
-                            .padding(2)
-                            .background(Color.secondary)
-                            .cornerRadius(min(geometry.size.width, geometry.size.height) * 0.3)
+            ScrollView(showsIndicators: false) {
+                VStack {
+                    Spacer()
+                        .frame(height: geometry.size.height * 0.1)
+                    Cover(album: self.album, size: min(geometry.size.width, geometry.size.height) * 0.6)
+                    Text(.init(self.album.subtitle))
+                        .foregroundColor(.secondary)
+                        .font(.footnote)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    ForEach(self.album.tracks, id: \.self) {
+                        Item(track: $0)
                     }
-                    Spacer().frame(width: min(geometry.size.width, geometry.size.height) * 0.2)
+                }
+            }.navigationBarTitle(.init(self.album.title))
+        }
+    }
+}
+
+private struct Cover: View {
+    let album: Album
+    let size: CGFloat
+    
+    var body: some View {
+        Image(album.cover)
+            .resizable()
+            .scaledToFill()
+            .frame(width: size - 2,
+                   height: size - 2)
+            .cornerRadius((size - 2) / 2)
+            .padding(2)
+            .background(Color.blue)
+            .cornerRadius(size / 2)
+    }
+}
+
+private struct Item: View {
+    let track: Track
+    
+    var body: some View {
+        Button(action: {
+            
+        }) {
+            VStack {
+                HStack {
+                    Text(.init(track.title))
+                        .font(.footnote)
+                    Spacer()
+                }
+                HStack {
+                    Text(.init(track.composer.name))
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                    Spacer()
                 }
             }
         }
